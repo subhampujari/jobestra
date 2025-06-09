7264d82499d40e50ab6e2b4058b3cd7805f2b4dc
Subham <subhampujari541@gmail.com.>
2025-06-09 20:20:01 +0530

    ggg

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { MantineProvider } from '@mantine/core';
 
 function App() {
   return (
      <MantineProvider >
-       <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+       <h1 className="text-3xl font-bold underline">   Hello world!</h1>
      </MantineProvider>
   );
 }
